0c1dcb3f83f173a2f98c27d834e16c99e0e1011a
Oscar <101546850+hirvain@users.noreply.github.com>
2022-05-23 01:46:51 -0500

    Gráfico
    Mejora en el cómo se muestra el grafo

--- dijkstra.py
+++ dijkstra.py
@@ -5,12 +5,11 @@ Created on Wed May 18 02:59:32 2022
 @author: Oscar Antonio García Avila
 """
 import networkx as nx
-import matplotlib.pyplot as plt
 import random
 import math
 #                  0                      1                   2                   3                          4                      5                       6                         7                     8                       9                       10                        11                       12                    13                      14                      15
-pesos = [random.randint(10, 20),random.randint(10, 20),random.randint(5, 10), random.randint(20, 30),random.randint(10, 30),random.randint(7, 12), random.randint(5, 10), random.randint(25, 40), random.randint(20, 30), random.randint(12, 20), random.randint(30, 50), random.randint(10, 20), random.randint(12, 20), random.randint(20, 30), random.randint(20, 30), random.randint(40, 60)]
-vertices = ["Casa","Auto","C85-A","T18-A","Caminar desde Providencia","C135", "Caminar desde Americas", "Caminar a Real Center", "C54", "C72", "T08", "Caminar Nueva Escocia", "Bicicleta", "CETI" ]
+pesos = [random.randint(10, 20),random.randint(20, 60),random.randint(5, 10), random.randint(20, 30),random.randint(10, 30),random.randint(7, 12), random.randint(5, 10), random.randint(25, 40), random.randint(20, 30), random.randint(12, 20), random.randint(30, 50), random.randint(10, 20), random.randint(12, 20), random.randint(20, 30), random.randint(20, 30), random.randint(40, 60)]
+vertices = ["Casa","Auto","C85-A","T18-A","Providencia","C135", "Americas", "R.Center", "C54", "C72", "T08", "N.Escocia", "Bicicleta", "CETI" ]
 """
 0 Casa
 1 Auto
@@ -33,7 +32,7 @@ for i in range(12):
     Grafo.add_node(vertices[i])  
 
 Grafo.add_edge(vertices[0],vertices[1], weight=1, color='b') 
-Grafo.add_edge(vertices[1],vertices[2], weight=pesos[1], color='b') 
+Grafo.add_edge(vertices[1],vertices[13], weight=pesos[1], color='b') 
 
 
 Grafo.add_edge(vertices[0],vertices[2], weight=pesos[2], color='b')
@@ -60,12 +59,13 @@ Grafo.add_edge(vertices[10],vertices[11], weight=pesos[14], color='b')
 Grafo.add_edge(vertices[0],vertices[12], weight=1, color='b')
 Grafo.add_edge(vertices[12],vertices[13], weight=pesos[15], color='b')
 
+#"""
 #Matriz de adyacencia
 
         #0          1          2            3           4          5          6         7           8         9         10          11        12            13
 mat =  [[0,         1,          pesos[2],  0,           0,         pesos[6],  0,        1,          0,        0,        0,          0,        1,            0     ], #0
-        [1,         0,          pesos[1],  0,           0,         0,         0,        0,          0,        0,        0,          0,        0,            0     ], #1
-        [pesos[2],  pesos[1],   0,         pesos[3],    0,         0,         0,        0,          0,        0,        0,          0,        0,            0     ], #2
+        [1,         0,          0,         0,           0,         0,         0,        0,          0,        0,        0,          0,        0,            pesos[1]], #1
+        [pesos[2],  0,          0,         pesos[3],    0,         0,         0,        0,          0,        0,        0,          0,        0,            0     ], #2
         [0,         0,          pesos[3],  0,           pesos[4],  0,         0,        0,          0,        0,        0,          0,        0,            0     ], #3
         [0,         0,          0,         pesos[4],    0,         0,         0,        0,          0,        0,        0,          0,        0,            pesos[5]], #4
         [pesos[6],  0,          0,         0,           0,         0,         pesos[7], 0,          0,        0,        0,          0,        0,            0     ], #5
@@ -76,7 +76,7 @@ mat =  [[0,         1,          pesos[2],  0,           0,         pesos[6],  0,
         [0,         0,          0,         0,           0,         0,         0,        0,          0,        pesos[13],0,          pesos[14],0,            0     ], #10
         [0,         0,          0,         0,           0,         0,         0,        0,          pesos[10],0,        pesos[14],  0,        0,            pesos[11]], #11
         [1,         0,          0,         0,           0,         0,         0,        0,          0,        0,        0,          0,        0,            pesos[15]], #12
-        [0,         0,          0,         0,           pesos[5],  0,         pesos[8], 0,          0,        0,        0,          pesos[13],pesos[15],    0     ]] #13
+        [0,         pesos[1],   0,         0,           pesos[5],  0,         pesos[8], 0,          0,        0,        0,          pesos[13],pesos[15],    0     ]] #13
 
 
 
@@ -117,16 +117,13 @@ def dijkstra(m, inicio, fin=-1):
     return (dist[fin], cami[fin]) if fin >= 0 else (dist, cami)
 
 
-
-
-
 pesofin,camino= dijkstra(mat, 0, 13)
 
 for i in range(len(camino)-1):
     Grafo.add_edges_from ([(vertices[camino[i]],vertices[camino[i+1]])], color = 'red')
     
 
-
+#"""
 
 """
 #networkx
@@ -139,22 +136,22 @@ print(camino)
 for i in range(len(camino)-1):
     Grafo.add_edges_from ([(camino[i],camino[i+1])], color = 'red')
 """ 
+
 colors = nx.get_edge_attributes(Grafo,'color').values()
 
-pos = nx.spring_layout(Grafo)
-plt.figure()
+coords = {"Casa":(-5,0),"Auto":(0,5),"C85-A":(-2,1.5),"T18-A":(0,2.5),"Providencia":(2,1.5),"C135":(-1.2,0),"Americas":(1.2,0),"R.Center":(-2.5,-1),"C54":(0,-1.5),"C72":(-1,-2.8),"T08":(1,-2.8),"N.Escocia":(2.5,-1),"Bicicleta":(0,-5),"CETI":(5,0)}
 
 
 nx.draw(
-    Grafo, pos, edge_color=colors, width=1, linewidths=1,
-    node_size=500, node_color='pink', alpha=0.9,
+    Grafo, pos= coords, edge_color=colors, width=2, linewidths=2,
+    node_size=1500, node_color='pink', alpha=1,
     labels={node: node for node in Grafo.nodes()}
 )
 
 nx.draw_networkx_edge_labels(
-    Grafo, pos,
+    Grafo, pos= coords,
     edge_labels={(vertices[0],vertices[1]): 1,
-                 (vertices[1],vertices[2]): pesos[1], 
+                 (vertices[1],vertices[13]): pesos[1], 
                  
                  (vertices[0],vertices[2]): pesos[2],
                  (vertices[2],vertices[3]): pesos[3],
@@ -182,6 +179,7 @@ nx.draw_networkx_edge_labels(
                  },
     font_color='red'
 )
+
 
 for n in camino:
     print(vertices[n])
